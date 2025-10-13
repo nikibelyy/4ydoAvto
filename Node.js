035343518cc2758b39fc41dc5a38680eafc5a431
nikibelyy <ogurtsov.1999@gmail.com>
2025-10-13 16:32:59 +0300

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,13 @@
+{
+  "name": "yandex-traffic-scraper",
+  "version": "1.0.0",
+  "description": "Scrape Yandex Probki page for accidents and send Telegram messages",
+  "main": "scraper.js",
+  "scripts": {
+    "start": "node scraper.js"
+  },
+  "dependencies": {
+    "puppeteer": "^21.0.0",
+    "node-fetch": "^3.3.1"
+  }
+}
